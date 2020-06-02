a1d0bc29a27d742bfaa4472f914b22e9fa4da3db
github username <github email address>
2020-06-02 01:01:15 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+export default class CoordinatesButton extends React.Component {
+
+    handleClick = event => {
+        this.props.onReceiveCoordinates([event.clientX, event.clientY])
+    }
+
+    render() {
+        return(
+            <button onClick={this.handleClick}>Coords</button>
+        )
+    }
+}
+
+
+// omponent takes in one prop: onReceiveCoordinates. This prop is a function 
+// passed down from index.js. This function is 
+// currently just logging whatever is passed into it.
